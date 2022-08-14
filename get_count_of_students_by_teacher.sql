aab168a2e1bf7d1ef4d7c7a67074fabfce0f3001
Your Name <deva162501@gmail.com>
2022-08-14 11:56:50 +0530

    get count of students in malar teacher class

---
+++ get_count_of_students_by_teacher.sql
@@ -0,0 +1,14 @@
+-- ---- 16) Get count of subject taken by malar --------------------------
+
+select teach.name as teacher,count(st.name) as no_of_students
+from teachers teach
+join teacher_subject_mapping tsm
+on teach.id = tsm.teach_id
+join subjects sub
+on tsm.sub_id = sub.id
+join student_subject_mapping ssm
+on sub.id = ssm.sub_id
+join student st
+on ssm.stu_id = st.id
+where teach.name like '%malar%'
+group by teach.name;
