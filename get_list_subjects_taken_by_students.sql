9efc01bd1b816cac5d539cf3903c72ce16445467
Your Name <deva162501@gmail.com>
2022-08-16 14:57:24 +0530

    list students and their subjects

---
+++ get_list_subjects_taken_by_students.sql
@@ -0,0 +1,9 @@
+-- 3) get the list of subjects taken each students
+
+select st.name as students, group_concat(sub.name) as subjects 
+from student st
+join student_subject_mapping ssm
+on st.id = ssm.stu_id
+join subjects sub
+on ssm.sub_id = sub.id
+group by st.name ;
